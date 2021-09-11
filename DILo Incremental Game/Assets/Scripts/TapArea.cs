661bb91bba9920d85e8ba2e8d7e81d4eafaa23b1
Adi Wibowo <adiwibowo0317@gmail.com>
2021-09-11 23:51:22 +0700

    Tugas 2 Incremental Game
    Tugas Chapter 2 AGATE

---
+++ DILo Incremental Game/Assets/Scripts/TapArea.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+using UnityEngine.EventSystems;
+
+ 
+
+public class TapArea : MonoBehaviour, IPointerDownHandler
+
+{
+
+    public void OnPointerDown (PointerEventData eventData)
+
+    {
+
+        GameManager.Instance.CollectByTap (eventData.position, transform);
+
+    }
+
+}
